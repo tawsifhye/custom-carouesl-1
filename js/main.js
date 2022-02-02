89ad338ec7036174a90aec4e2704770b30cd7a12
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-02-02 17:19:15 +0600

    pagination nex prev added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const itializeSlide = () => {
+
+}
+
 $(document).ready(() => {
 	const itemsPerPage = 3;
 	const numberOfItems = $('.product').length;
@@ -10,7 +14,7 @@ $(document).ready(() => {
 	let navigation_html = '<a class="previous_link" href="javascript:previous();">Prev</a>';
 	let current_link = 0;
 	while (numberOfPages > current_link) {
-		navigation_html += '<a class="page_link" href="javascript:go_to_page(' + current_link + ')" longdesc="' + current_link + '">' + (current_link + 1) + '</a>';
+		navigation_html += '<a class="page_link" href="javascript:goToPage(' + current_link + ')" value="' + current_link + '">' + (current_link + 1) + '</a>';
 		current_link++;
 		console.log(navigation_html);
 	}
@@ -19,10 +23,37 @@ $(document).ready(() => {
 	$('#page_navigation').html(navigation_html);
 
 	$('#page_navigation .page_link:first').addClass('active_page');
+	$('#slider-container').children().css('display', 'none');
+	$('#slider-container').children().slice(0, itemsPerPage).css('display', 'block')
 })
 
+const previous = () => {
+	const newPage = parseInt($('#current_page').val()) - 1;
+	if ($('.active_page').prev('.page_link').length == true) {
+		goToPage(newPage);
+	}
+}
+
+const next = () => {
+	const newPage = parseInt($('#current_page').val()) + 1;
+	if ($('.active_page').next('.page_link').length == true) {
+		goToPage(newPage);
+	}
+}
 
 
+const goToPage = (pageNum) => {
+
+	let itemsPerPage = parseInt($('#show_per_page').val())
+	let startFrom = pageNum * itemsPerPage;
+	// console.log(startFrom);
+	let endOn = startFrom + itemsPerPage;
+	// console.log(endOn);
+
+	$('#slider-container').children().css('display', 'none').slice(startFrom, endOn).css('display', 'block');
+	$('.page_link[value=' + pageNum + ']').addClass('active_page').siblings('.active_page').removeClass('active_page');
+	$('#current_page').val(pageNum);
+}
 
 
 
